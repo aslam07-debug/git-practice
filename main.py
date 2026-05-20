91547447881c88e6c5a33b3c1e48c870d56af483
Abdul-Rahman Aslam <aslamabdulrahaman@gmail.com>
2026-05-20 23:39:21 +0500

    added some new changes

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     #some awesome feature for feature-1
+    #some new changes in main
     name = input("Enter your name")
     print_hi('name')
 
